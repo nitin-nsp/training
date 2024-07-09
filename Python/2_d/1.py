33522e75f32ece6458a07a1439ef40966e897ccc
nitin-nsp <nitin.singh@habilelabs.io>
2024-07-09 10:47:02 +0530

    python:init

---
+++ Python/2_d/1.py
@@ -0,0 +1,22 @@
+# a=[1,2,3,4,5,6,7,8,9,10]
+
+# print(a[-1::-1])
+# print(a[::-1])
+
+# challenge = '123'
+# print(challenge.isdecimal())  # True
+
+# Define a variable
+x = 5
+
+# Create a string with the superscript two character
+output = f"x\u00B2 = {x}\u00B2"
+
+# Print the result
+print(output)
+
+str='Coding For All string'.split(' ')
+print(str,str[0])
+
+str="Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon".split('e, ')
+print(str)
